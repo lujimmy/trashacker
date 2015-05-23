0f3e1e0eea237e52712bf0a867c010d8b09b6e9b
CarolRan <carolran1979@gamil.com>
2015-05-23 13:56:00 +0800

    Add search by type & location+time;

--- src/main/java/org/trashacker/service/SearchCollectPointService.java
+++ src/main/java/org/trashacker/service/SearchCollectPointService.java
@@ -2,9 +2,11 @@ package org.trashacker.service;
 
 import java.sql.Time;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.trashacker.data.domain.GarbageType;
 import org.trashacker.data.domain.PointType;
 import org.trashacker.repository.ClothesRecyclingBoxRepo;
 import org.trashacker.repository.GarbageTruckRoadmapRepo;
@@ -30,31 +32,78 @@ public class SearchCollectPointService {
 	@Autowired
 	GarbageTruckRoadmapRepo garbageTruckRoadmapRepo;
 
-	public SearchResultBean searchAllTypePointsByLocation(float maxLat, float minLat, float maxLng, float minLng){
+	public SearchResultBean searchAllTypePointsByLocation(List<String> queryTypes, float maxLat, float minLat, float maxLng, float minLng){
 		SearchResultBean result = new SearchResultBean();
 		
-		result.setClothesPoints(clothesRecyclingBoxRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
-		result.setDrugsPoints(medicationDisposalSiteRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
-		result.setFixedPoints(recyclingFoodWasteDepotRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
-		result.setFoodScrapsPoints(wedSunFoodWasteDepotRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
-		result.setGarbageCar(garbageTruckRoadmapRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+		if (queryTypes.contains(GarbageType.GENERAL.getName()) || queryTypes.contains(GarbageType.RECYCLE.getName()) || queryTypes.contains(GarbageType.FOOD_SCRAP.getName())){
+			result.setFixedPoints(recyclingFoodWasteDepotRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+			result.setGarbageCar(garbageTruckRoadmapRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+		}
+		
+		if (queryTypes.contains(GarbageType.FOOD_SCRAP.getName())){
+			result.setFoodScrapsPoints(wedSunFoodWasteDepotRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+		}
+		
+		if (queryTypes.contains(GarbageType.DRUG.getName())){
+			result.setDrugsPoints(medicationDisposalSiteRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+		}
+		
+		if (queryTypes.contains(GarbageType.CLOTHES.getName())){
+			result.setClothesPoints(clothesRecyclingBoxRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+		}
+				
+		return result;
+	}
+	
+	public SearchResultBean searchAllTypePointsByTime(List<String> queryTypes, Time arrive, Time leave){
+		SearchResultBean result = new SearchResultBean();
+		
+		GregorianCalendar queryStartTime = new GregorianCalendar(0,0,0,arrive.toLocalTime().getHour(),leave.toLocalTime().getHour());
+		
+		if (queryTypes.contains(GarbageType.GENERAL.getName()) || queryTypes.contains(GarbageType.RECYCLE.getName()) || queryTypes.contains(GarbageType.FOOD_SCRAP.getName())){
+			result.setGarbageCar(garbageTruckRoadmapRepo.getByTimeRange(arrive, leave));
+			
+			if (queryStartTime.after(PointType.FIXED_POINT.getStartTime()) && queryStartTime.before(PointType.FIXED_POINT.getEndTime())){
+				result.setFixedPoints(recyclingFoodWasteDepotRepo.findAll());
+			}
+			
+		}
+		
+		if (queryTypes.contains(GarbageType.FOOD_SCRAP.getName())){
+			if (queryStartTime.after(PointType.FOOD_SCRAPS_ONLY_POINT.getStartTime()) && queryStartTime.before(PointType.FOOD_SCRAPS_ONLY_POINT.getEndTime())){
+				result.setFoodScrapsPoints(wedSunFoodWasteDepotRepo.findAll());
+			}
+		}
 		
 		return result;
 	}
 	
-	public SearchResultBean searchAllTypePointsByTime(Time arrive, Time leave){
+	public SearchResultBean searchByTimeAndLocation(List<String> queryTypes, float maxLat, float minLat, float maxLng, float minLng,Time arrive, Time leave){
 		SearchResultBean result = new SearchResultBean();
 		
-		result.setGarbageCar(garbageTruckRoadmapRepo.getByTimeRange(arrive, leave));
-		
 		GregorianCalendar queryStartTime = new GregorianCalendar(0,0,0,arrive.toLocalTime().getHour(),leave.toLocalTime().getHour());
-
-		if (queryStartTime.after(PointType.FIXED_POINT.getStartTime()) && queryStartTime.before(PointType.FIXED_POINT.getEndTime())){
-			result.setFixedPoints(recyclingFoodWasteDepotRepo.findAll());
+		
+		if (queryTypes.contains(GarbageType.GENERAL.getName()) || queryTypes.contains(GarbageType.RECYCLE.getName()) || queryTypes.contains(GarbageType.FOOD_SCRAP.getName())){
+			
+			result.setGarbageCar(garbageTruckRoadmapRepo.getByLocationAndTimeRange(maxLat, minLat, maxLng, minLng, arrive, leave));
+			
+			if (queryStartTime.after(PointType.FIXED_POINT.getStartTime()) && queryStartTime.before(PointType.FIXED_POINT.getEndTime())){
+				result.setFixedPoints(recyclingFoodWasteDepotRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+			}
 		}
 		
-		if (queryStartTime.after(PointType.FOOD_SCRAPS_ONLY_POINT.getStartTime()) && queryStartTime.before(PointType.FOOD_SCRAPS_ONLY_POINT.getEndTime())){
-			result.setFoodScrapsPoints(wedSunFoodWasteDepotRepo.findAll());
+		if (queryTypes.contains(GarbageType.FOOD_SCRAP.getName())){
+			if (queryStartTime.after(PointType.FOOD_SCRAPS_ONLY_POINT.getStartTime()) && queryStartTime.before(PointType.FOOD_SCRAPS_ONLY_POINT.getEndTime())){
+				result.setFoodScrapsPoints(wedSunFoodWasteDepotRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+			}
+		}
+		
+		if (queryTypes.contains(GarbageType.DRUG.getName())){
+			result.setDrugsPoints(medicationDisposalSiteRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
+		}
+		
+		if (queryTypes.contains(GarbageType.CLOTHES.getName())){
+			result.setClothesPoints(clothesRecyclingBoxRepo.getByLocationRange(maxLat, minLat, maxLng, minLng));
 		}
 		
 		return result;
